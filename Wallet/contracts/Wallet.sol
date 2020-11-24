f7a6524a8c86e3d0504c7f666dee14c49238f39a
moranthomas <thomas@optimizme.com>
2020-11-24 11:19:53 +0000

    contract - callable methods

--- Wallet/contracts/Wallet.sol
+++ Wallet/contracts/Wallet.sol
@@ -4,35 +4,37 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol;
 
 interface IYDAI {
-    function deposit(uint _amount) external;
-    function withdraw(uint256 _shares) external;
-    function balanceOf(address account) external view returns (uint);
-    function getPricePerFullShare() external view returns (uint);
+  function deposit(uint _amount) external;
+  function withdraw(uint _amount) external;
+  function balanceOf(address _address) external view returns(uint);
+  function getPricePerFullShare() external view returns(uint);
 }
 
 contract Wallet {
+address admin;
 
-    address admin;
     IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     IYDAI yDai = IYDAI(0xC2cB1040220768554cf699b0d863A3cd4324ce32);
 
     uint public data = 9;
     uint public balance = 0;
 
+    mapping (address => uint) balances;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     constructor() public {
         admin = msg.sender;
         balances[tx.origin] = 10000;
     }
 
-    function payMe() payable public returns(bool success) {
-        balance += msg.value;
-        return true;
-    }
-
-
     function save(uint amount) external {
         dai.transferFrom(msg.sender, address(this), amount);
         _save(amount);
+    }
+
+    function _save(uint amount) internal {
+        dai.approve(address(yDai), amount);
+        yDai.deposit(amount);
     }
 
     function spend(uint amount, address recipient) external {
@@ -47,11 +49,7 @@ contract Wallet {
         }
     }
 
-    function _save(uint amount) internal {
-        dai.approve(address(yDai), amount);
-        yDai.deposit(amount);
-    }
-
+    // Renamed to avoid conflict with balance variable when calling
     function balanceFunction() public view returns(uint) {
         uint price = yDai.getPricePerFullShare();
         uint balanceShares = yDai.balanceOf(address(this));
@@ -60,10 +58,11 @@ contract Wallet {
 
 
 
-
-    mapping (address => uint) balances;
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    /* Extra Callable */
+    function payMe(address sender) payable public returns(bool success) {
+        balance += msg.value;
+        return true;
+    }
 
     function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
         if (balances[msg.sender] < amount) return false;
