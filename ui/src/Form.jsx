feb5833e59139375038075d232cc3a62d6d02d68
moranthomas <thomas@optimizme.com>
2020-11-24 20:38:15 +0000

    create contract from config

--- ui/src/Form.jsx
+++ ui/src/Form.jsx
@@ -1,16 +1,20 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 import React, { Component } from 'react';
 import Web3 from 'web3';
 //import { contractJSON } from '../../Wallet/build/contracts/Wallet.json';
+import { contractAbi, contractAddress } from './config';
+
 
 export class Form extends Component {
 
     componentWillMount() {
-        this.loadBlockchainData();
-        this.getContractABI('../../Wallet/build/contracts/Wallet.json');
 
-        var WalletContract = new web3.eth.Contract(contractAbi, contractAddress);
-        console.log(WalletContract);
+        this.loadBlockchainData();
+        //this.getContractABI('../../Wallet/build/contracts/Wallet.json');
+        this.getContractAbiFromConfig();
+        this.getWalletAddressFromConfig();
+        this.createContract();
+        
     }
 
     constructor(props) {
@@ -22,7 +26,7 @@ export class Form extends Component {
         this.state = {
             account: '',
             contractABI: '',
-            contractAddress: '0x778f2614776fB677965d0E4eb1Ae9803C64bCd56',
+            contractAddress: '',
         };
     }
 
@@ -44,12 +48,30 @@ export class Form extends Component {
         this.setState({ account: accounts[0] });
     }
 
+    getContractAbiFromConfig() {
+        this.setState( {contractAbi: this.contractABI});
+    }
+
+    getWalletAddressFromConfig() {
+        this.setState( {contractAddress: this.contractAddress});
+    }
+
     getContractABI(JsonFile) {
         //const contractJSON = JSON.parse(fs.readFileSync(JsonFile, 'utf8'));
         const abiString = JSON.stringify(JsonFile.abi);
         console.log(JsonFile.abi);
-        const abi = [ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "_from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "_to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "_value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [], "name": "balance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [], "name": "data", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "save", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "address", "name": "recipient", "type": "address" } ], "name": "spend", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "balanceFunction", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "payMe", "outputs": [ { "internalType": "bool", "name": "success", "type": "bool" } ], "stateMutability": "payable", "type": "function", "payable": true }, { "inputs": [ { "internalType": "address", "name": "receiver", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "sendCoin", "outputs": [ { "internalType": "bool", "name": "sufficient", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "addr", "type": "address" } ], "name": "getBalanceInEth", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "address", "name": "addr", "type": "address" } ], "name": "getBalance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true } ];
+        const abi = contractAbi;
         this.setState({ contractABI: abi });
+    }
+
+    async createContract() {
+        const ganacheUrl = "http://localhost:8545";
+        let web3Provider = new Web3.providers.HttpProvider(ganacheUrl);
+        const web3 = new Web3(web3Provider);
+        const contractInstance  = new web3.eth.Contract(contractAbi, contractAddress);
+        let contractMethods = await contractInstance.methods;
+        console.log(contractMethods);
+        return contractInstance;
     }
 
     render() {
